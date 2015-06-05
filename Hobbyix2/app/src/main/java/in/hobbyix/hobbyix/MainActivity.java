d506cf35db1736c75a2f5413038613de322e711a
Yashdeep Sharma <sharma.yashdeep9@gmail.com>
2015-06-05 17:55:31 +0530

    yashdeep

--- Hobbyix2/app/src/main/java/in/hobbyix/hobbyix/MainActivity.java
+++ Hobbyix2/app/src/main/java/in/hobbyix/hobbyix/MainActivity.java
@@ -1,5 +1,6 @@
 package in.hobbyix.hobbyix;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -102,8 +103,9 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater mif=getMenuInflater();
-        mif.inflate(R.menu.menu_main,menu);
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 }
