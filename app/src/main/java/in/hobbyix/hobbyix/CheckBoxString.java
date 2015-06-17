2d67b4f8d82856541aae8e0faf56100e40cf6288
diksha ahuja <dikshaa42@gail.com>
2015-06-17 14:17:46 +0530

    applied onclick listener

--- app/src/main/java/in/hobbyix/hobbyix/CheckBoxString.java
+++ app/src/main/java/in/hobbyix/hobbyix/CheckBoxString.java
@@ -1,14 +1,36 @@
 package in.hobbyix.hobbyix;
 
+import android.widget.CheckBox;
+
 /**
  * Created by Yashdeep Sharma on 06-06-2015.
  */
 public class CheckBoxString {
-    private String mainString;
-    public CheckBoxString(String mainstring){
-        mainString=mainstring;
+
+    String code = null;
+    String name = null;
+    boolean selected = false;
+
+    public CheckBoxString( String name, boolean selected) {
+        super();
+
+        this.name = name;
+        this.selected = selected;
     }
-    public String getMainString(){
-        return mainString;
+
+
+    public String getName() {
+        return name;
     }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+
 }
--- app/src/main/java/in/hobbyix/hobbyix/FilterPage.java
+++ app/src/main/java/in/hobbyix/hobbyix/FilterPage.java
@@ -1,26 +1,39 @@
 package in.hobbyix.hobbyix;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class FilterPage extends ActionBarActivity {
-    private List<CheckBoxString> checkBoxList = new ArrayList<CheckBoxString>();
-    private List<CheckBoxString> checkBoxList2 = new ArrayList<CheckBoxString>();
-    String load_locality_details[]=new String[100];
-    String load_subcategory_details[]=new String[100];
+    MyListAdapter adapter;
+    MyListAdapter2 adapter2;
+
+
+    String load_locality_details[] = new String[100];
+    String load_subcategory_details[] = new String[100];
+    ArrayList<CheckBoxString> checkBoxList = new ArrayList<CheckBoxString>();
+    ArrayList<CheckBoxString> checkBoxList2 = new ArrayList<CheckBoxString>();
+
+    Button reset, filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,64 +42,285 @@ public class FilterPage extends ActionBarActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setHomeButtonEnabled(true);
         actionBar.setDisplayHomeAsUpEnabled(true);
+        reset = (Button) findViewById(R.id.ResetButton);
+        filter = (Button) findViewById(R.id.FilterButton);
         load_locality_details = new Location_batches().store_details_of_locality();
-        load_subcategory_details=new Subcategory_Fitness().store_details_of_subcategory();
+        load_subcategory_details = new Subcategory_Fitness().store_details_of_subcategory();
         populatecheckBoxList();
-        populateListView();
+
+        checkButtonClick();
 
     }
-    private void populatecheckBoxList(){
 
+    public void onclickreset(View view) {
 
-        for(int i=0;i<Integer.valueOf(load_subcategory_details[0]);i++) {
-            checkBoxList.add(new CheckBoxString(load_subcategory_details[i+1]));
-        }
-        for(int i=0;i<Integer.valueOf(load_locality_details[0]);i++) {
-            checkBoxList2.add(new CheckBoxString(load_locality_details[i+1]));
-        }
     }
-    private void populateListView() {
-        ArrayAdapter<CheckBoxString> adapter = new MyListAdapter();
+
+
+    public void populatecheckBoxList() {
+
+
+        for (int i = 0; i < Integer.valueOf(load_subcategory_details[0]); i++) {
+            checkBoxList.add(new CheckBoxString(load_subcategory_details[i + 1], false));
+        }
+        for (int i = 0; i < Integer.valueOf(load_locality_details[0]); i++) {
+            checkBoxList2.add(new CheckBoxString(load_locality_details[i + 1], false));
+        }
+        adapter = new MyListAdapter(this,
+                R.layout.checkbox_element, checkBoxList);
         ListView list = (ListView) findViewById(R.id.SubCategoriesListView);
         list.setAdapter(adapter);
-        ArrayAdapter<CheckBoxString> adapter2 = new MyListAdapter2();
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+
+                // When clicked, show a toast with the TextView text
+                CheckBoxString checkbox1 = (CheckBoxString) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Clicked on Row: " + checkbox1.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        adapter2 = new MyListAdapter2(this,
+                R.layout.checkbox_element, (ArrayList<CheckBoxString>) checkBoxList2);
         ListView list2 = (ListView) findViewById(R.id.LocalityListView);
         list2.setAdapter(adapter2);
+
+        list2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                CheckBoxString checkbox2 = (CheckBoxString) parent.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(),
+                        "Clicked on Row: " + checkbox2.getName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
+
+
     private class MyListAdapter extends ArrayAdapter<CheckBoxString> {
-        public MyListAdapter() {
-            super(FilterPage.this, R.layout.checkbox_element, checkBoxList);
+        private final ArrayList<CheckBoxString> checkboxlist;
+        boolean[] checkBoxState;
+
+
+        public MyListAdapter(Context context, int textViewResourceId,
+                             ArrayList<CheckBoxString> checkboxlist) {
+            super(context, textViewResourceId, checkboxlist);
+            checkBoxState = new boolean[200];
+            this.checkboxlist = new ArrayList<CheckBoxString>();
+            this.checkboxlist.addAll(checkboxlist);
+
+        }
+
+        private class ViewHolder {
+
+            CheckBox name;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            View itemView = convertView;
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.checkbox_element, parent, false);
+        public View getView(final int position, View convertView, ViewGroup parent) {
+
+            ViewHolder holder = null;
+            Log.v("ConvertView", String.valueOf(position));
+            //CheckBoxString checkBoxString = new CheckBoxString();
+            if (convertView == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+
+                convertView = vi.inflate(R.layout.checkbox_element, null);
+
+
+                holder = new ViewHolder();
+                holder.name = (CheckBox) convertView.findViewById(R.id.ItemCheckBox);
+
+
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v;
+                        CheckBoxString checkBoxxx = (CheckBoxString) cb.getTag();
+                        Toast.makeText(getApplicationContext(),
+                                "Clicked on Checkbox: " + cb.getText() +
+                                        " is " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        checkBoxxx.setSelected(cb.isChecked());
+                    }
+                });
+
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
             }
-            CheckBoxString CurrentString=checkBoxList.get(position);
-            CheckBox checkbox = (CheckBox)itemView.findViewById(R.id.ItemCheckBox);
-            checkbox.setText(CurrentString.getMainString());
-            return itemView;
+
+            CheckBoxString checkBoxString1 = checkboxlist.get(position);
+
+
+            holder.name.setText(checkBoxString1.getName());
+            holder.name.setChecked(checkBoxString1.isSelected());
+            holder.name.setTag(checkBoxString1);
+            return convertView;
+
         }
+
     }
+
+
     private class MyListAdapter2 extends ArrayAdapter<CheckBoxString> {
-        public MyListAdapter2() {
-            super(FilterPage.this, R.layout.checkbox_element, checkBoxList);
+
+        private final ArrayList<CheckBoxString> checkboxlist;
+        boolean[] checkBoxState;
+
+
+        public MyListAdapter2(Context context, int textViewResourceId,
+                              ArrayList<CheckBoxString> checkboxlist) {
+            super(context, textViewResourceId, checkboxlist);
+            checkBoxState = new boolean[200];
+            this.checkboxlist = new ArrayList<CheckBoxString>();
+            this.checkboxlist.addAll(checkboxlist);
+
+        }
+
+        private class ViewHolder {
+            CheckBox name;
         }
 
         @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+        public View getView(final int position, View convertView, ViewGroup parent) {
             View itemView = convertView;
-            if (itemView == null) {
-                itemView = getLayoutInflater().inflate(R.layout.checkbox_element, parent, false);
+            ViewHolder holder = null;
+            //CheckBoxString checkBoxString = new CheckBoxString();
+            if (convertView == null) {
+                convertView = getLayoutInflater().inflate(R.layout.checkbox_element, null);
+
+
+                holder = new ViewHolder();
+                holder.name = (CheckBox) convertView.findViewById(R.id.ItemCheckBox);
+
+                convertView.setTag(holder);
+
+                holder.name.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        CheckBox cb = (CheckBox) v;
+                        CheckBoxString checkBoxxx = (CheckBoxString) cb.getTag();
+                        Toast.makeText(getApplicationContext(),
+                                "Clicked on Checkbox: " + cb.getText() +
+                                        " is " + cb.isChecked(),
+                                Toast.LENGTH_LONG).show();
+                        checkBoxxx.setSelected(cb.isChecked());
+                    }
+                });
+
+
+            } else {
+                holder = (ViewHolder) convertView.getTag();
             }
-            CheckBoxString CurrentString=checkBoxList2.get(position);
-            CheckBox checkbox = (CheckBox)itemView.findViewById(R.id.ItemCheckBox);
-            checkbox.setText(CurrentString.getMainString());
-            return itemView;
+
+            CheckBoxString checkBoxString1 = checkboxlist.get(position);
+
+            holder.name.setText(checkBoxString1.getName());
+            holder.name.setChecked(checkBoxString1.isSelected());
+            holder.name.setTag(checkBoxString1);
+            return convertView;
+
         }
+
     }
+
+    public void checkButtonClick() {
+
+        Button filter = (Button) findViewById(R.id.FilterButton);
+        Button reset=(Button) findViewById(R.id.ResetButton);
+
+        filter.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("Selected Countries are...\n");
+
+                ArrayList<CheckBoxString> checkboxlists = adapter.checkboxlist;
+
+                for (int i = 0; i < checkboxlists.size(); i++) {
+                    CheckBoxString check = checkboxlists.get(i);
+
+                    if (check.isSelected()) {
+                        responseText.append("\n" + check.getName());
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), responseText,
+                        Toast.LENGTH_LONG).show();
+
+
+                ArrayList<CheckBoxString> checkboxlists2 = adapter2.checkboxlist;
+
+                for (int i = 0; i < checkboxlists2.size(); i++) {
+                    CheckBoxString check = checkboxlists2.get(i);
+
+                    if (check.isSelected()) {
+                        responseText.append("\n" + check.getName());
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), responseText,
+                        Toast.LENGTH_LONG).show();
+            }
+
+
+        });
+        reset.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("Selected Countries are...\n");
+
+                ArrayList<CheckBoxString> checkboxlists = adapter.checkboxlist;
+
+                for (int i = 0; i < checkboxlists.size(); i++) {
+                    CheckBoxString check = checkboxlists.get(i);
+
+                    if (check.isSelected()) {
+                        check.setSelected(false);
+                    }
+                }
+
+                Toast.makeText(getApplicationContext(), responseText,
+                        Toast.LENGTH_LONG).show();
+
+
+                ArrayList<CheckBoxString> checkboxlists2 = adapter2.checkboxlist;
+
+                for (int i = 0; i < checkboxlists2.size(); i++) {
+                    CheckBoxString check = checkboxlists2.get(i);
+
+                    if (check.isSelected()) {
+                        check.setSelected(false);
+                    }
+                }
+
+
+                Toast.makeText(getApplicationContext(), responseText,
+                        Toast.LENGTH_LONG).show();
+                Intent OpenLogin = new Intent(v.getContext(), MainActivity.class);
+
+                startActivityForResult(OpenLogin, 0);
+            }
+
+
+        });
+    }
+
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -99,3 +333,5 @@ public class FilterPage extends ActionBarActivity {
         }
     }
 }
+
+
